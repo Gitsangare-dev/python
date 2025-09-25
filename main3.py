6f10b91304d8b33313d1655030042b3ab808da8f
falix <falix290@gmail.com>
2025-09-25 19:06:46 +0200

    push 3

---
+++ main3.py
@@ -0,0 +1,11 @@
+nom = "falix"
+age = 35
+taille = 1.75
+est_etudiant = True
+
+print(f"Je m'appelle {nom} j'ai {age} ans. Je fais {taille} m et l'ordinateur valide le fais que je sois etudiant : {est_etudiant} .")
+
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(est_etudiant))
