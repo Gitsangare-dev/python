3880dec5056df5e08ebdd2a3b6afff8b0a8e9c9c
falix <falix290@gmail.com>
2025-09-25 21:10:05 +0200

    push

---
+++ main4.py
@@ -0,0 +1,14 @@
+fruits = ["pomme", "banane", "orange"]
+print(fruits)
+
+fruits.append("kiwi")
+print(fruits)
+
+fruits.remove("orange")
+print(fruits)
+
+fruits[1] = "ananas"
+print(fruits)
+
+fruits.sort()
+print(fruits)
